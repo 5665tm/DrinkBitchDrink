3eaf802a31ad8a4139958cbdbcbd6b56b618120b
5665tm <5665tm@gmail.com>
2014-03-04 21:31:33 +0400

    second commit

--- Assets/Scripts/DrinkBITCHES.cs
+++ Assets/Scripts/DrinkBITCHES.cs
@@ -1,5 +1,7 @@
-﻿// Created : 2014 03 02 2:39 PM : DrinkBitchDrink-csharp : Assembly-CSharp-vs
-// Changed : 2014 03 04 6:07 PM : Вадим Караваев
+﻿// Created : 2014 03 02 2:39 PM : drink-bitch-drink-csharp : Assembly-CSharp-vs
+// Changed : 2014 03 04 9:09 PM : Вадим Караваев
+
+//#define DEVEL
 
 using UnityEngine;
 using Random = System.Random;
@@ -8,6 +10,7 @@ using Random = System.Random;
 public class DrinkBITCHES : MonoBehaviour
 {
 	public AudioClip Shot;
+	public GameObject Txt2;
 	private int _count;
 
 	private void FixedUpdate()
@@ -23,25 +26,46 @@ public class DrinkBITCHES : MonoBehaviour
 			if (x == 42)
 			{
 #if DEVEL
-				_count = 5;
+				_count = 10;
 #else
 				_count = 60;
 #endif
 				TextMesh txtRunGame = gameObject.GetComponent("TextMesh") as TextMesh;
-				txtRunGame.text = "DRINK![" + _count + "]";
+				TextMesh txtTxt2 = Txt2.GetComponent("TextMesh") as TextMesh;
+				txtTxt2.fontSize = 300;
+				txtRunGame.fontSize = 300;
+				txtRunGame.text = "DRINK!";
+				txtTxt2.text = "DRINK!";
 				audio.PlayOneShot(Shot);
 			}
 			else
 			{
 				TextMesh txtRunGame = gameObject.GetComponent("TextMesh") as TextMesh;
-				txtRunGame.text = "число[" + x + "]";
+				TextMesh txtTxt2 = Txt2.GetComponent("TextMesh") as TextMesh;
+				txtTxt2.fontSize = 300;
+				txtRunGame.fontSize = 300;
+				txtRunGame.text = "=[" + x + "]=";
+				txtTxt2.text = "=[" + x + "]=";
 			}
+		}
+
+#if DEVEL
+		else if (_count > 5)
+#else
+		else if (_count > 50)
+#endif
+		{
+			_count--;
 		}
 		else
 		{
 			_count--;
 			TextMesh txtRunGame = gameObject.GetComponent("TextMesh") as TextMesh;
-			txtRunGame.text = "DRINK![" + _count + "]";
+			TextMesh txtTxt2 = Txt2.GetComponent("TextMesh") as TextMesh;
+			txtTxt2.fontSize = 200;
+			txtRunGame.fontSize = 200;
+			txtRunGame.text = "wait " + _count;
+			txtTxt2.text = "wait " + _count;
 		}
 	}
 }
